598675bc772a6a13e976d19385b7d32f1c67231b
Stanley Stuart <stanley@stan.li>
2014-01-06 10:17:59 -0600

    add tests, make sure the binding actually works

--- test/support/templates.js
+++ test/support/templates.js
@@ -1,23 +1,19 @@
 Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1, hashTypes, hashContexts, options, escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing;
+  var buffer = '', stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
   data.buffer.push("<p>Year range should only be 2013 to 2014</p>\n\n<p>Date is: ");
-  hashTypes = {};
-  hashContexts = {};
-  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "date", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  stack1 = helpers._triageMustache.call(depth0, "date", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("</p>\n\n");
-  hashContexts = {'value': depth0,'yearRange': depth0,'onSelect': depth0,'onClose': depth0};
-  hashTypes = {'value': "ID",'yearRange': "STRING",'onSelect': "STRING",'onClose': "STRING"};
-  options = {hash:{
+  data.buffer.push(escapeExpression((helper = helpers['jqui-datepicker'] || (depth0 && depth0['jqui-datepicker']),options={hash:{
     'value': ("date"),
     'yearRange': ("2013:2014"),
     'onSelect': ("datepickerSelect"),
     'onClose': ("datepickerClosed")
-  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers['jqui-datepicker'] || (depth0 && depth0['jqui-datepicker'])),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "jqui-datepicker", options))));
+  },hashTypes:{'value': "ID",'yearRange': "STRING",'onSelect': "STRING",'onClose': "STRING"},hashContexts:{'value': depth0,'yearRange': depth0,'onSelect': depth0,'onClose': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "jqui-datepicker", options))));
   data.buffer.push("\n\n");
   return buffer;
   
